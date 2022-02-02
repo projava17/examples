ba1f32f5360d78512ae60fcfbe3e869673cd3536
Yusuke Yamamoto <yusuke@mac.com>
2022-02-02 16:59:32 +0900

    spring initializr

---
+++ part-6/chapter-21-spring-boot/src/main/java/jp/gihyo/projava/tasklist/TaskListApplication.java
@@ -0,0 +1,13 @@
+package jp.gihyo.projava.tasklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskListApplication.class, args);
+	}
+
+}
