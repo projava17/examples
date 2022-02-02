d262f45a4b692f2106be996ba4ad86abda31d80b
Yusuke Yamamoto <yusuke@mac.com>
2022-02-02 22:47:17 +0900

    Thymeleafを使う

--- part-6/chapter-21-spring-boot/src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ part-6/chapter-21-spring-boot/src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -1,24 +1,16 @@
 package jp.gihyo.projava.tasklist;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
 
 @Controller
 public class HomeController {
     @RequestMapping(value = "/hello")
-    @ResponseBody
-    String hello() {
-        return """
-                <html>
-                    <head><title>Hello</title></head>
-                    <body>
-                        <h1>Hello</h1>
-                        It works!<br>
-                        現在時刻は%sです。</body>
-                    </html>
-                """.formatted(LocalDateTime.now());
+    String hello(Model model) {
+        model.addAttribute("time", LocalDateTime.now());
+        return "hello";
     }
 }
