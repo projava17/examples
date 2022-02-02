2659d4b0a3026e8010f7fb01f8eb1e870179ec50
Yusuke Yamamoto <yusuke@mac.com>
2022-02-02 17:19:13 +0900

    restlist作成

--- part-6/chapter-21-spring-boot/src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
+++ part-6/chapter-21-spring-boot/src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -1,18 +1,48 @@
 package jp.gihyo.projava.tasklist;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 public class HomeRestController {
-    @RequestMapping(value="/resthello")
-    String hello(){
+
+    record TaskItem(String id, String task, String deadline, boolean done) {}
+
+    private List<TaskItem> taskItems = new ArrayList<>();
+
+    @RequestMapping(value = "/resthello")
+    String hello() {
         return """
                 Hello,
                 It works!
                 現在時刻は%sです。
                 """.formatted(LocalDateTime.now());
     }
+
+    @GetMapping("/restadd")
+    String adItem(@RequestParam("task") String task,
+                  @RequestParam("deadline") String deadline) {
+        String id = UUID.randomUUID().toString().substring(0, 8);
+        TaskItem item = new TaskItem(id, task, deadline, false);
+        taskItems.add(item);
+
+        return "タスクを追加しました";
+    }
+
+    @GetMapping("/restlist")
+    String listItems(){
+        String result = taskItems.stream()
+                .map(TaskItem::toString)
+                .collect(Collectors.joining(", "));
+        return result;
+    }
+
 }
