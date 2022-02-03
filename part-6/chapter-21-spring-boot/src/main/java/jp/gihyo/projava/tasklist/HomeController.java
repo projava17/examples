ab72e408b1ee7e228cbcbf916d31581657a17616
Yusuke Yamamoto <yusuke@mac.com>
2022-02-03 18:00:42 +0900

    Thymeleafを使ってaddとlist

--- part-6/chapter-21-spring-boot/src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ part-6/chapter-21-spring-boot/src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -2,15 +2,40 @@ package jp.gihyo.projava.tasklist;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class HomeController {
+    record TaskItem(String id, String task, String deadline, boolean done) {
+    }
+
+    private List<TaskItem> taskItems = new ArrayList<>();
+
     @RequestMapping(value = "/hello")
     String hello(Model model) {
         model.addAttribute("time", LocalDateTime.now());
         return "hello";
+    }
+
+    @GetMapping(value = "/list")
+    String listItems(Model model) {
+        model.addAttribute("tasklist", taskItems);
+        return "home";
+    }
+
+    @GetMapping( "/add")
+    String addItem(@RequestParam("task") String task,
+                   @RequestParam("deadline") String deadline) {
+        String id = UUID.randomUUID().toString().substring(0, 8);
+        TaskItem item = new TaskItem(id, task, deadline, false);
+        taskItems.add(item);
+        return "redirect:/list";
     }
 }
